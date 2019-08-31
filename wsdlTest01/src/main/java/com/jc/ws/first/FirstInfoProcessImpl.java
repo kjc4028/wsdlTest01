67033d6d503e98002a1de345b9fc3611bd86b99c
Administrator <Administrator@jongin-PC>
2019-09-01 03:50:43 +0900

    wsdl 전문 확인 및 프로젝트 정상 구동 확인

--- wsdlTest01/src/main/java/com/jc/ws/first/FirstInfoProcessImpl.java
+++ wsdlTest01/src/main/java/com/jc/ws/first/FirstInfoProcessImpl.java
@@ -7,7 +7,6 @@ public class FirstInfoProcessImpl implements FirstInfoProcess {
 	@Override
 	public FirstVo processFirst() {
 		FirstVo vo = new FirstVo();
-		System.out.println("웹서비스 호출");
 		vo.setUser("jckim");
 		return vo;
 	}
